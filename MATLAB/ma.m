9a12072c943f6d320fd6733d6cc5c2f2bacd9f2b
Ronan Cailleau <ro.cailleau@gmail.com>
2019-11-28 08:31:08 +0100

    Fixed ma.m with single instead of double

--- MATLAB/ma.m
+++ MATLAB/ma.m
@@ -1,34 +1,35 @@
 clear all;
 close all;
 
-video = VideoReader('../data/face.mp4');
+file = 'face';
+
+video = VideoReader(strcat('../data/', file, '.mp4'));
 
 fps = video.FrameRate;
 video_len = int32(fps * video.Duration);
-frames = cell(video_len, 1);
+width = video.Width;
+height = video.Height;
+frames = single(zeros(video_len, height, width, 3));
 
 i = 1;
 while(hasFrame(video))
     frame = video.readFrame();
-    frames{i} = double(frame) / 255;
+    frames(i,:,:,:) = single(frame) / 255;
     i = i + 1;
 end
 clear video;
-frames = cell2mat(frames);
-frames = single(frames);
-
 'video read'
 tic
 fourier = single(fft(frames, [], 1));
 
-freq = 0.3; % Hz
+freq = 0.6; % Hz
 bandwidth = 0.0;
 band = bandwidth * freq;
 
 alpha = 50;
 
-low = floor(fps * (freq - band)) + 1;
-high = ceil(fps * (freq + band)) + 1;
+low = floor(fps * (freq - band) / 2) + 1;
+high = ceil(fps * (freq + band) / 2)  + 1;
 
 low = max(1, low)
 high = min(video_len, high)
@@ -39,8 +40,16 @@ end
 
 
 
-frames=real(ifft(fourier, [], 1));
+frames=single(real(ifft(fourier, [], 1)));
+
+frames(frames <= 0) = 0;
+
 toc
+
+'saving video'
+%vw = VideoWriter(strcat('result/',file, '.mp4'));
+
+
 i = 1;
 while(true)
     frame = squeeze(frames(i, :, :, :));
