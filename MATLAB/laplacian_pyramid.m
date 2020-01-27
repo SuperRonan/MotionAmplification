342159265ef1548ddf2b042491d984b5f0192ec9
Ronan Cailleau <ro.cailleau@gmail.com>
2020-01-27 09:34:37 +0100

    Added reconstruct function

--- MATLAB/laplacian_pyramid.m
+++ MATLAB/laplacian_pyramid.m
@@ -4,5 +4,6 @@ function [list] = laplacian_pyramid(gaussian_pyramid)
     for i = 1 : N-1
         list{i} = single(gaussian_pyramid{i}) - single(imresize(impyramid(gaussian_pyramid{i+1}, 'expand'), [size(gaussian_pyramid{i}, 1), size(gaussian_pyramid{i}, 2)]));
     end
+    list{N} = gaussian_pyramid{N};
 end
 
