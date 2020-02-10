9ecee18ea78dd975b736d0780598d28f6bd825c1
Ronan Cailleau <ro.cailleau@gmail.com>
2020-02-10 09:38:43 +0100

    Fixed uncorrect assert

--- MATLAB/weighted_merge.m
+++ MATLAB/weighted_merge.m
@@ -1,5 +1,5 @@
 function res = weighted_merge(pyramid, amplified_pyramid, alphas)
-assert(all(size(alphas) == size(pyramid)));
+assert(all(size(alphas) >= size(pyramid)));
 assert(all(size(pyramid) == size(amplified_pyramid)));
 
 N = size(pyramid, 2);
